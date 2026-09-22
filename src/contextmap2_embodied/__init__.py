be120851ff165415ef51e5005c66e3f041baed74
Alexandre Tortoza <85268093+Alexandre-Tortoza@users.noreply.github.com>
2026-09-22 16:20:32 -0300

    bootstrap: add src/contextmap2_embodied/__init__.py

---
+++ src/contextmap2_embodied/__init__.py
@@ -0,0 +1,25 @@
+"""ContextMap2 embodied downstream validation package."""
+
+from contextmap2_embodied.contracts import (
+    EntitySelector,
+    MissionPlan,
+    NavigateTo,
+    RelationConstraint,
+    Repeat,
+    ResolveTarget,
+    SemanticSelector,
+    TargetQuery,
+)
+
+__all__ = [
+    "EntitySelector",
+    "MissionPlan",
+    "NavigateTo",
+    "RelationConstraint",
+    "Repeat",
+    "ResolveTarget",
+    "SemanticSelector",
+    "TargetQuery",
+]
+
+__version__ = "0.1.0.dev0"
